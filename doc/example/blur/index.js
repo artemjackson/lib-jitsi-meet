5f8d899457859107745a28317457446d0efa1a58
Artyom Stepanishchev <artem.stepanishchev@banuba.com>
2021-02-08 21:25:40 +0200

    Added JitsiStreamBlurEffect
    Copied from https://github.com/jitsi/jitsi-meet/tree/master/react/features/stream-effects/blur

---
+++ doc/example/blur/index.js
@@ -0,0 +1,29 @@
+// https://github.com/jitsi/jitsi-meet/blob/master/react/features/stream-effects/blur/index.js
+
+/* global bodyPix */
+// import * as bodyPix from '@tensorflow-models/body-pix';
+
+import JitsiStreamBlurEffect from './JitsiStreamBlurEffect.js';
+
+/**
+ * Creates a new instance of JitsiStreamBlurEffect. This loads the bodyPix model that is used to
+ * extract person segmentation.
+ *
+ * @returns {Promise<JitsiStreamBlurEffect>}
+ */
+export async function createBlurEffect() {
+    if (!MediaStreamTrack.prototype.getSettings && !MediaStreamTrack.prototype.getConstraints) {
+        throw new Error('JitsiStreamBlurEffect not supported!');
+    }
+
+    // An output stride of 16 and a multiplier of 0.5 are used for improved
+    // performance on a larger range of CPUs.
+    const bpModel = await bodyPix.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        multiplier: 0.50,
+        quantBytes: 2
+    });
+
+    return new JitsiStreamBlurEffect(bpModel);
+}
